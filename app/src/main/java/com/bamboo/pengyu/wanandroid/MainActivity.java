d4ba030966db5ea8851f103a87469550f5216870
pengyu <whut_bamboo@foxmail.com>
2019-01-17 23:53:20 +0800

    achive tabhost to switch fragment

--- app/src/main/java/com/bamboo/pengyu/wanandroid/MainActivity.java
+++ app/src/main/java/com/bamboo/pengyu/wanandroid/MainActivity.java
@@ -1,12 +1,17 @@
 package com.bamboo.pengyu.wanandroid;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements TabHost.OnTabChangeListener {
 
@@ -34,11 +39,30 @@ public class MainActivity extends FragmentActivity implements TabHost.OnTabChang
     }
 
     private View getTabView(int idx) {
-        return new View(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.footer_tabs,null);
+        ((TextView) view.findViewById(R.id.tv_tab_name)).setText(TabDB.getTabsTxt()[idx]);
+        if(idx == 0) {
+            ((TextView) view.findViewById(R.id.tv_tab_name)).setTextColor(Color.RED);
+            ((ImageView) view.findViewById(R.id.iv_tab_icon)).setImageResource(TabDB.getTabsImgLight()[idx]);
+        } else {
+            ((ImageView) view.findViewById(R.id.iv_tab_icon)).setImageResource(TabDB.getTabsImg()[idx]);
+        }
+        return view;
     }
 
     @Override
     public void onTabChanged(String tabId) {
-
+        TabWidget tabWidget = tabHost.getTabWidget();
+        for(int i=0; i<tabWidget.getChildCount(); i++) {
+            View view = tabWidget.getChildTabViewAt(i);
+            ImageView imageView = view.findViewById(R.id.iv_tab_icon);
+            if(i == tabHost.getCurrentTab()) {
+                ((TextView) view.findViewById(R.id.tv_tab_name)).setTextColor(Color.RED);
+                imageView.setImageResource(TabDB.getTabsImgLight()[i]);
+            } else {
+                ((TextView) view.findViewById(R.id.tv_tab_name)).setTextColor(Color.BLACK);
+                imageView.setImageResource(TabDB.getTabsImg()[i]);
+            }
+        }
     }
 }
--- app/src/main/java/com/bamboo/pengyu/wanandroid/fragment/KnowledgeFragment.java
+++ app/src/main/java/com/bamboo/pengyu/wanandroid/fragment/KnowledgeFragment.java
@@ -1,8 +1,23 @@
 package com.bamboo.pengyu.wanandroid.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
  * Created by pengyu on 2019/1/16.
  */
 
-public class KnowledgeFragment {
+public class KnowledgeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        TextView textView = new TextView(getContext());
+        textView.setText("知识体系");
+        return textView;
+    }
 }
